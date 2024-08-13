429cf14e8d8876a63464b76ece019747b3ce6b6f
Vane <vane.moralesn@gmail.com>
2024-08-12 21:04:39 -0400

    se corrige texto

--- filtro.py
+++ filtro.py
@@ -1,11 +1,11 @@
 import sys
 precios = {
-    "notebook": 700000,
-    "teclado": 25000,
-    "mouse": 12000,
-    "monitor": 250000,
-    "escritorio": 135000,
-    "tarjeta de video": 1500000
+    "Notebook": 700000,
+    "Teclado": 25000,
+    "Mouse": 12000,
+    "Monitor": 250000,
+    "Escritorio": 135000,
+    "Tarjeta de video": 1500000
 }
 if len(sys.argv) < 2 or len(sys.argv) > 3: 
     print("Debes especificar un umbral y opcionalmente una condición.")
@@ -16,3 +16,11 @@ else:
         print("El umbral debe ser número")
     else:
         condicion = "mayor" if len(sys.argv) == 2 else sys.argv[2].lower()
+        if condicion == "mayor":
+            productos_filtrados = {producto: precio for producto, precio in precios.items() if precio > umbral}
+            print("Los productos mayores al umbral son: ", ",".join(productos_filtrados.keys()))
+        elif condicion == "menor":  
+            productos_filtrados = {producto: precio for producto, precio in precios.items() if precio < umbral}  
+            print("Los productos menores al umbral son:", ', '.join(productos_filtrados.keys()))
+        else:
+            print("Lo sentimos, no es una operación válida")
