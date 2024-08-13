ef943156b2adef3df05d42ad91faa06ce32130ee
Nicole Pinilla <npinilla89@gmail.com>
2024-08-13 16:55:47 -0400

    se crea funcion resultado_errores y se ordena codigo existente

--- filtro.py
+++ filtro.py
@@ -19,21 +19,35 @@ def filtrado_por_precio(precios, umbral, condicion):
         return "Lo sentimos, no es una operación válida"
 
 
+def resultado_errores():
+    if len(sys.argv) < 2: 
+        print("Debes especificar un umbral y opcionalmente una condición, ejemplo python filtro.py umbral(valor) mayor/menor")
+        return
 
-if len(sys.argv) < 2 or len(sys.argv) > 3: 
-    print("Debes especificar un umbral y opcionalmente una condición.")
-else:
     try:
         umbral =int(sys.argv[1])
     except ValueError:
         print("El umbral debe ser número")
+        return
+
+    
+    condicion = 'mayor'
+    if len(sys.argv) == 3:
+        condicion = sys.argv[2].lower()
+
+    precio_filtrado = filtrado_por_precio(precios, umbral, condicion)
+
+    if isinstance(precio_filtrado, str):
+        print(precio_filtrado)
     else:
-        condicion = "mayor" if len(sys.argv) == 2 else sys.argv[2].lower()
-        if condicion == "mayor":
-            productos_filtrados = {producto: precio for producto, precio in precios.items() if precio > umbral}
-            print("Los productos mayores al umbral son: ", ",".join(productos_filtrados.keys()))
-        elif condicion == "menor":  
-            productos_filtrados = {producto: precio for producto, precio in precios.items() if precio < umbral}  
-            print("Los productos menores al umbral son:", ', '.join(productos_filtrados.keys()))
-        else:
-            print("Lo sentimos, no es una operación válida")
+        productos = ', '.join(precio_filtrado.keys())
+        if condicion == 'mayor':
+            print(f"Los productos mayores al umbral son: {productos}")
+        elif condicion == 'menor':
+            print(f"Los productos menores al umbral son: {productos}")
+
+if __name__ == "__main__":
+    resultado_errores()
+
+
+
